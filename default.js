20b410bd89f48b9eecc449e5787d9bfd1fbf7923
Douglas Crisona <douglascrisona@gmail.com>
2016-06-29 14:52:14 -0700

    Sends name and zip to backend and retrieves menu object. #1

--- default.js
+++ default.js
@@ -2,7 +2,8 @@ var button = document.getElementById('search-button');
 
 button.addEventListener('click', function(e) {
 var searchTerm = document.getElementById('search')
-  restSearch = venueSearch(searchTerm.value)
+var searchZip = document.getElementById('postal')
+  restSearch = venueSearch(searchTerm.value, searchZip.value)
   console.log(restSearch)
 
 
@@ -10,18 +11,34 @@ var searchTerm = document.getElementById('search')
   xhr.open('POST', '/restaurants/');
   xhr.setRequestHeader('Content-Type', 'application/json')
   xhr.send(JSON.stringify(restSearch))
-  console.log(restSearch)
-/**
+  //console.log(restSearch)
+
   xhr.onload = function() {
-    console.log(xhr.responseText)
+    //console.log(JSON.parse(xhr.responseText))
+    results = JSON.parse(xhr.responseText)
+    results.forEach(function(result) {
+      result.venues.forEach(function(venue) {
+        console.log(venue.name, venue.location.locality, venue.location.address1)
+        venue.menus.forEach(function(name) {
+          console.log(name.menu_name)
+          console.log(name.sections)
+        });
+      });
+    });
   }
-**/
 
 });
 
 
-function venueSearch(name) {
+function venueSearch(name, zip) {
   theSearch = {};
-  theSearch.id = name;
+  theSearch.name = name;
+  theSearch.zip = zip;
   return theSearch;
 }
+
+
+
+// Type of route
+// How to use the request module
+// How to access the returned data
