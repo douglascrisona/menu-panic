25c441a133bbf7beb88a63a6d6e2688b40134805
Douglas Crisona <douglascrisona@gmail.com>
2016-07-05 09:29:40 -0700

    Users object. #2

---
+++ users.js
@@ -0,0 +1,18 @@
+var users = [
+  {
+    name: 'Doug',
+    password: 1234,
+    choices: [
+      { items: ['sauce'] }
+    ]
+  },
+  {
+    name: 'Bob',
+    password: 5678,
+    choices: [
+      { items: [] }
+    ]
+  },
+]
+
+module.exports = users;
